c48fdbf1ea0dbc44e49d35e4979c7e48f835ec19
richie.buckingham <richie.buckingham@gmail.com>
2018-08-19 13:38:07 +1000

    retrieve values

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
@@ -10,18 +11,26 @@ namespace DatingApp.API.Controllers
   [ApiController]
   public class ValuesController : ControllerBase
   {
+    private readonly DataContext context;
+
+    public ValuesController(DataContext context)
+    {
+      this.context = context;
+    }
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public IActionResult GetValues()
     {
-      return new string[] { "value1", "value2" };
+      var values = this.context.Values.ToList();
+      return Ok(values);
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    public ActionResult<string> GetValue(int id)
     {
-      return "value";
+      var value = this.context.Values.FirstOrDefault(v => v.Id == id);
+      return Ok(value);
     }
 
     // POST api/values
