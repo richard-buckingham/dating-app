5d85165af7c0ae79e6f4fd3f9837cd79a916090a
richie.buckingham <richie.buckingham@gmail.com>
2018-08-19 11:49:39 +1000

    add initial migration

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -24,13 +24,10 @@ namespace DatingApp.API
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
+    // This method gets called by the runtime. Use this method to add services to the container.ks
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddDbContext<DataContext>(x =>
-      {
-        x.UseSqlite("ConnectionString");
-      });
+      services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
     }
 
